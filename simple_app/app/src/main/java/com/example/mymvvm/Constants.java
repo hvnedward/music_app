2c10c4461585cf3040f4a432171213406e1178b9
hvnedward <dhduc@humaxdigital.com>
2021-10-04 10:12:21 +0700

    add comment

--- simple_app/app/src/main/java/com/example/mymvvm/Constants.java
+++ simple_app/app/src/main/java/com/example/mymvvm/Constants.java
@@ -1,7 +1,7 @@
 package com.example.mymvvm;
 
 public class Constants {
-
+    //channel id of notification
     public static final  String CHANNEL_NAME ="name_notification";
     public static final String SONG_KEY = "song";
     public static final int NOTIFICATION_ID = 1;
